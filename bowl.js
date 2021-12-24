12619400161ac9562486af38f3e11b13385ef8ce
Vaishnavi-pix <77741780+Vaishnavi-pix@users.noreply.github.com>
2021-12-24 10:08:03 -0500

    Add files via upload

---
+++ bowl.js
@@ -0,0 +1,43 @@
+image1="";
+status="";
+object=[];
+function preload(){
+    image1=loadImage("Bowl.jpg");
+}
+function setup(){
+    canvas=createCanvas(620,550);
+    canvas.center();
+    
+    objectDetecter=ml5.objectDetector("cocossd",modelLoaded);
+}
+function modelLoaded(){
+console.log("Model Loaded");
+status=true;
+objectDetecter.detect(image1,gotResult);
+}
+function gotResult(error,results){
+if(error){
+    console.log(error);
+}
+else{
+    console.log(results);
+    object=results;
+}
+}
+function draw(){
+    image(image1,0,0,620,550);
+    if(status != ""){
+        r=random(255);
+        g=random(255);
+        b=random(255);
+        for(i=0;i<object.length;i++){
+            
+            percent=floor(object[i].confidence *100);
+            fill(r,g,b);
+            text(object[i].label+" "+percent+"%",object[i].x+15,object[i].y+15);
+            noFill();
+            stroke(r,g,b);
+            rect(object[i].x,object[i].y,object[i].width,object[i].height);
+        }
+    }
+}
